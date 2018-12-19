80823896f11fdf7f8a7d8432a1eaa880615e09fc
Sven Eliasson <sven@rysta.io>
2018-12-19 21:34:47 +0100

    Day 6, Part 2, C++

--- 06/solution.cpp
+++ 06/solution.cpp
@@ -5,6 +5,14 @@
 #include <cstdlib>
 #include <algorithm>
 
+/**** POST MORTEM
+As I started this challange I thought a object oriented approach would
+help much more than it did. The solution was much simpler than expected, 
+so the classes added more boilerplate than needed. 
+So if I would write this again, I would not introduce classes into this
+and solve it in much fewer lines.
+*///
+
 using std::string; 
 using std::vector;
 
@@ -21,8 +29,8 @@ class Point {
     std::string toString() const{
       return std::string(std::to_string(x) + std::string(", ") + std::to_string(y));
     }
-    const int x;
-    const int y;
+    int x;
+    int y;
 };
 
 class Field {
@@ -31,33 +39,23 @@ class Field {
       _size(size), 
       _field(size, vector<char>(size, '.')){}
 
-    bool addPoint(const Point p, char id) {
+    void addPoint(const Point p, char id) {
       _dest.push_back({p, id});
       _field[p.y][p.x] = id;
     }
-
-    bool print(){
+    void print(){
       for(auto &row : _field){
-        for( auto &entry : row){
+        for( auto &entry : row)
           std::cout << entry << " ";
-        }
         std::cout << "\n";
       }
     }
-    void printDest(){
-      std::cout << "\n";
-      for(auto &p: _dest){
-        std::cout << p.second << ": " << p.first.toString() << "\n";
-      }
-    }
-
-    bool calcShortestDistance(){
+    void calcShortestDistance(){
       for(int row=0; row < _size; row++){
         for(int col=0; col < _size; col++){
           auto distances = std::vector<int>();
-
           // actually the second loop through can be saved by accumulating max dist while calc dist, 
-          // this approach is less efficient, but more readable
+          // this approach is less efficient, but better to digest
           for( auto &p :_dest){
             const int d = p.first.distanceTo(Point(row,col));
             distances.push_back(d);
@@ -74,19 +72,15 @@ class Field {
         }
       }
     }
-
     vector<int> getAreas(){
       auto inf = getAllInfinite();
       vector<int> areas = {};
       for(auto &p: _dest){
-        // if point not in inf, calc area
         if( std::find(inf.begin(), inf.end(), p.second) == inf.end()){
           int count = 0;
           for(auto &row: _field){
             for(auto &e: row){
-              if( e == p.second){
-                count++;
-              }
+              if( e == p.second){ count++; }
             }
           }
           areas.push_back(count);
@@ -95,8 +89,7 @@ class Field {
       return areas;
     }
 
-
-  private:
+  protected:
     vector<char> getAllInfinite(){
       // a area is infinite if the selected char is on the edge
       auto addIfNotAlreadyAdded = [](vector<char> *v, char e){
@@ -104,24 +97,63 @@ class Field {
           v->push_back(e);
         }
       };
-      // left/right edge
       auto infinite = vector<char>({});
       for( int row=0; row<_size; row++){
         addIfNotAlreadyAdded(&infinite, _field[row][0]);
         addIfNotAlreadyAdded(&infinite, _field[row][_size-1]);
       }
-      // top/bottom
       for( int col=0; col<_size; col++) {
           addIfNotAlreadyAdded(&infinite,_field[0][col]);
           addIfNotAlreadyAdded(&infinite, _field[_size-1][col]);
       }
       return infinite;
     }
-
     int _counter = 0;
     const int _size = 10;
     vector<vector<char>> _field; 
     vector<std::pair<Point, char>> _dest = {};
+};
+
+
+class Field2 {
+  public: 
+    Field2( int size): _size(size), _field(size, vector<int>(size, '.')){};
+
+    void calculateSumOfAllDistances(){
+      for(int row=0; row < _size; row++){
+        for(int col=0; col < _size; col++){
+          _field[col][row] = 0;
+          for( auto &p :_dest){
+            _field[col][row] += p.distanceTo(Point(row,col));
+          }
+        }
+      }
+    };
+
+    bool addPoint(const Point p) {
+      _dest.push_back(p);
+    }
+
+    bool print(){
+      for(auto &row : _field){
+        for( auto &entry : row)
+          std::cout << entry << " ";
+        std::cout << "\n";
+      }
+    }
+    int64_t countSmallerThan(int x){
+      int64_t count = 0;
+      for(auto &row : _field){
+        for( auto &entry : row){
+          if( entry < x) count += 1;
+        }
+      }
+      return count;
+    }
+    vector<Point> _dest = {};
+    vector<vector<int>> _field;
+    const int _size;
+
 };
 
 /** @description reads the selected file into a std::vector<string> */
@@ -129,7 +161,6 @@ vector<Point> readInputFile(const string fileName){
   auto inputFile = std::ifstream(fileName);
   auto line = string();
   auto returnVector = vector<Point>();
-
   while( std::getline(inputFile, line)){
     if( line.length() > 0){
       returnVector.push_back(Point(line));
@@ -142,19 +173,24 @@ vector<Point> readInputFile(const string fileName){
 int64_t solutionPart1(){
   const auto coordinates = readInputFile("input.txt");
   Field field(365);
-  auto i = 'a';
+  auto i = 'a'; 
   for( auto &p: coordinates ){
     field.addPoint(Point(p), i++);
   }
   field.calcShortestDistance();
-  //field.print();
   auto allAreas = field.getAreas();
   auto maxArea =  *std::max_element(allAreas.begin(), allAreas.end());
   return maxArea;
 }
 
 int64_t solutionPart2(){
-
+  const auto coordinates = readInputFile("input.txt");
+  Field2 field(365);
+  for(auto &p: coordinates){
+    field.addPoint(p);
+  }
+  field.calculateSumOfAllDistances();
+  return field.countSmallerThan(10000);
 }
 
 int main(){
