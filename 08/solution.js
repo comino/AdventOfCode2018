56dc143a3bc2dee5cf398e62f445f8eb50b64e45
Sven Eliasson <sven@rysta.io>
2018-12-24 14:05:29 +0100

    Added day 08 part 2, javascript recursive

--- 08/solution.js
+++ 08/solution.js
@@ -17,13 +17,27 @@ const timeout = ms => new Promise(res => setTimeout(res, ms))
 function evalNode(arr, sum){
   const numNodes = arr.shift();
   const numMeta = arr.shift();
-  Array(numNodes).fill().forEach( e => {
-    sum = evalNode(arr, sum);
-  });
-  Array(numMeta).fill().forEach( e => {
-    sum += arr.shift();
-  })
+  Array(numNodes).fill().forEach( e => { sum = evalNode(arr, sum) });
+  Array(numMeta).fill().forEach( e => { sum += arr.shift() })
   return sum;
+}
+
+function evalNode2(arr, sum){
+  const numNodes = arr.shift();
+  const numMeta = arr.shift();
+  if(numNodes) {
+    let childW = Array(numNodes).fill().map( e => evalNode2(arr, sum));
+    sum = 0;
+    Array(numMeta).fill().forEach( e => {
+      let index = arr.shift();
+      sum += childW[index-1] ? childW[index-1]: 0;
+    });
+    return sum;
+  }else{
+    sum = 0;
+    Array(numMeta).fill().forEach( e =>{ sum+=arr.shift()})
+    return sum;
+  }
 }
 
 function solutionPart1() {
@@ -32,9 +46,9 @@ function solutionPart1() {
 }
 
 function solutionPart2() {
-  //const entries = getFileContent(INPUT_FILE).toString().split("\n");
-  return 0;
+  const entries = getFileContent(INPUT_FILE)
+  return evalNode2(entries, 0);
 }
 console.log("Solution- Part1: " + solutionPart1() ) ;
+console.log("Solution- Part2: " + solutionPart2() ) ;
 
-//console.log("Solution- Part2: " + solutionPart2());
